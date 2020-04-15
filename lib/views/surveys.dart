d93b79500e3b830b7469eb5add1174fb80c539bc
Sebastian <Sebastian@Strobl.net>
2020-04-15 23:42:59 +0200

    Add: Dynamic Question Input and Proofing

--- lib/views/surveys.dart
+++ lib/views/surveys.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 
@@ -90,17 +91,21 @@ class _SurveyPageState extends State<SurveyPage> {
 class SingleSurveyPage extends StatefulWidget {
   final survey;
 
-  SingleSurveyPage({this.survey});
+  SingleSurveyPage({Key key, @required this.survey}) : super(key: key);
 
   @override
-  _SingleSurveyPageState createState() => _SingleSurveyPageState();
+  _SingleSurveyPageState createState() => _SingleSurveyPageState(survey);
 }
 
 
 class _SingleSurveyPageState extends State<SingleSurveyPage> {
   bool _isLoading = false;
   var survey;
-  var survey_answer;
+  List optionsList;
+  List surveyAnswers;
+  List<TextEditingController> textController;
+
+  _SingleSurveyPageState (this.survey);
 
   _putAnswer() async {
     setState(() => {
@@ -117,26 +122,131 @@ class _SingleSurveyPageState extends State<SingleSurveyPage> {
       });
     }
   }
+  @override
+  void dispose() {
+    for(var textController in this.textController) {
+      textController.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.surveyAnswers = [for(var i=0; i<this.survey["questions"].length; i++) ""];
+    this.textController = [for(var i=0; i<this.survey["questions"].length; i++) new TextEditingController()];
+    this.optionsList = [for(var i=0; i<this.survey["questions"].length; i++) -1];
+    for (var controller in this.textController) {
+        controller.addListener(this.setTextValues);
+    }
+  }
+
+  void setTextValues() {
+    for (var i=0; i<this.survey["questions"].length; i++) {
+      if (this.survey["questions"][i]["type"] == "text" || this.survey["questions"][i]["type"] == "number") {
+        this.surveyAnswers[i] = textController[i].text;
+        setState(() {
+        });
+      }
+    }
+  }
+
+  bool checkCorrect() {
+    for (var i=0; i<this.survey["questions"].length; i++) {
+      if (this.survey["questions"][i]["type"] == "text" || this.survey["questions"][i]["type"] == "number"){
+        if (this.surveyAnswers[i] == ""){
+          return false;
+        }
+      }
+      if (this.survey["questions"][i]["type"] == "radio"){
+        if (this.surveyAnswers[i] == null || this.surveyAnswers[i] == "" || this.surveyAnswers[i] == -1){
+          return false;
+        }
+      }  
+    }
+    return true;
+  }
 
   @override
     Widget build(BuildContext context) {
-      setState(() => {
-        this.survey = widget.survey,
-      });
-      print('-----');
-      print(this.survey['questions'].length);
-      return new Scaffold(
+      return new Scaffold (
         appBar: new AppBar(
-          title: Text(widget.survey["name"]),
+          title: Text(this.survey["name"]),
         ),
-        body: Center(
-          child: new ListView.builder(
-            itemCount: this.survey["questions"] != null ? this.survey["questions"].length : 0,
-            itemBuilder: (context, i) {
-              final question = this.survey["questions"][i];
-              return new Text(question["question"]);
-            })
-        )
-      );
-    }
+        body: new Column (
+          children: <Widget>[
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: this.survey["questions"] != null ? this.survey["questions"].length : 0,
+                itemBuilder: (context, i) {
+                  final question = this.survey["questions"][i];
+                  return new Container(
+                    margin: const EdgeInsets.all(8.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new Container(
+                          alignment: Alignment(-1.0, 0.0),
+                          color: Colors.amber,
+                          child: new Text(question["question"] + (question["required"]? " *" : "")),
+                        ),
+                        if (question["type"] == "text")
+                          new Container(
+                            color: Colors.white,
+                            child: new TextField(
+                              controller: this.textController[i],
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Your Answer'
+                            ), 
+                            )
+                          )
+                        else if (question["type"] == "number")
+                          new Container(
+                            color: Colors.white,
+                            child: new TextField(
+                              controller: this.textController[i],
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                WhitelistingTextInputFormatter.digitsOnly
+                              ],
+                            )
+                          )
+                        else if (question["type"] == "radio")
+                          for (var option in question["options"])
+                            new RadioListTile<int>(
+                              title: new Text(option["name"]),
+                              value: option["value"],
+                              groupValue: this.optionsList[i],
+                              onChanged: (int value) {
+                                setState(() => {
+                                  this.optionsList[i] = value,
+                                  this.surveyAnswers[i] = value.toString(),
+                                  print(this.surveyAnswers)
+                                });
+                              }
+                            )
+                        else
+                          new Text("Invalid / No Type given")
+                      ],
+                    )
+                  ); 
+                },
+              ),
+            ),
+          
+          new RaisedButton(
+            onPressed: checkCorrect()? () => {
+              print("To send:"),
+              print(this.surveyAnswers),
+              print("--------------"),
+            } : null,
+            child: Text(
+              'Submit',
+              style: TextStyle(fontSize: 20)
+            ),
+          ),
+        ],
+      )
+    );
+  }
 }
